0e65d89c4416b448ea9e407b7d1fbdcd8aa85b71
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-09-26 19:25:17 +0000

    vamos

--- Estructuras de Datos Vectores (1D)/3.2.py
+++ Estructuras de Datos Vectores (1D)/3.2.py
@@ -4,10 +4,17 @@ inverso."""
 import array as arr
 mi_arreglo=arr.array('i',[0]*10)
 mi_segundo_arreglo=arr.array('i',[0]*10)
+print(mi_arreglo)
+print(mi_segundo_arreglo)
+print(len(mi_arreglo))
+print(type(mi_arreglo))
+print(mi_arreglo[0])
+print(mi_arreglo[1])
+print(mi_arreglo[9])
 for i in range(len(mi_arreglo)):
-    mi_arreglo[i]=int(input(f"ingrese valor {i+1} : " ))
-    
+    mi_arreglo[i]=int(input(f"ingrese numero {i+1} : "))
+
 mi_segundo_arreglo=mi_arreglo[::-1]
-     
+        
 print(mi_arreglo) 
-print(mi_segundo_arreglo)      
+print(mi_segundo_arreglo)   
