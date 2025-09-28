f31b43d1cbdce5c1ea6cc28dc5cf9f2e5d6b6ee1
cristian alejandro perez <mandymoorejcpaz@gmail.com>
2025-09-28 20:37:13 +0000

    ya hice lio borre todo pero no inporta voy practicando !!!

--- Estructuras de Datos Vectores (1D)/evaluacion_curso_anterior.py
+++ Estructuras de Datos Vectores (1D)/evaluacion_curso_anterior.py
@@ -16,38 +16,29 @@ en el resto de posiciones copiar el valor 0. **** re loco !!! no se hace
 
 Salir del programa  menos mal!!! jajaj hdp"""
 
+
 import array as arr
 mi_arreglo=arr.array('i',[0]*3)
-print(len(mi_arreglo))
-for i in range(len(mi_arreglo)):
-    carga_de_valores=int(input(f"ingrese valor entre -100 y 100  ingrese valor {i+1} : "))
-    if carga_de_valores>=-100 and carga_de_valores<=100:
-        mi_arreglo[i]=carga_de_valores
-      
-print("listo arreglo ")
+i=0
+while i<len(mi_arreglo):
+    carga=int(input(f"ingrese numeros entre -100 y 100 {i+1} : "))
+    if carga>=-100 and carga<=100:
+        mi_arreglo[i]=carga
+        i+=1
+    else:
+        print("valor no admitido ")
 
-     
-opcion=0
-while opcion !=7:
-    print("**************mi menu *******************")
-    print("1 para mostrar  numeros cargados (todos) ")#listo
-    print("2 para realizar al sumatoria de todos los valores positivos inpares ")
-    print("3 realizar el conteo de todos los valores negativos ")
-    print("4 calcular el promedio de todos los valores positivos ingresados ")
-    print("5 obtener el numero negativo que mas veces se repite en el vector ,mostrar aviso en caso de que no se hayan ingresado numeros negativos ")
-    print("6 generar un vector destino y copiar  en las mismas posiciones ")
-    print("7 salir del programa ")
-    opcion=int(input("que desea hacer lcdtm?")) 
-    if opcion==7:
-        print("gracias por utlizar el programa ")
-        break 
-    elif opcion==1:
-        print(mi_arreglo)
-    elif opcion==2:
-        arreglo_positivos_inpares=[numero for numero in mi_arreglo if  numero >0 and numero %2!= 0]#jajaja no me peque
-        suma_arreglo_positivos_inpares=sum(arreglo_positivos_inpares)
-        print(f"opcion 2 la suma es {suma_arreglo_positivos_inpares}")
     
+
+
+print(f"el arreglo cargado es : {mi_arreglo }")
+
+
+
+
+
+
+
 
         
 
